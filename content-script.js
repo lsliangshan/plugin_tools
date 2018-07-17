16c52f58d53c85917fce6ebec51bb27066d5cd90
梁山 <liangshan@192.168.0.107>
2018-07-18 01:55:46 +0800

    Init

---
+++ content-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('.............')
+  // document.body.style.backgroundColor = "cyan";
+  document.body.innerHTML = '<p style="color: darkcyan; font-size: 40px;">测试</p>'
+})
