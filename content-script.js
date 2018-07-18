7fc910445565a93d6f19e27021fe983871d8fb26
liangshan <loveliangshan@126.com>
2018-07-18 16:22:08 +0800

    no message

--- content-script.js
+++ content-script.js
@@ -1,5 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('.............')
-  // document.body.style.backgroundColor = "cyan";
-  document.body.innerHTML = '<p style="color: darkcyan; font-size: 40px;">测试</p>'
+  // document.body.innerHTML = '<p style="color: darkcyan; font-size: 40px;">测试</p>'
+  let content = document.body.innerText
+  let result = ''
+  if (content !== '') {
+    try {
+      JSON.parse(content)
+    } catch (e) {
+      return
+    }
+    insertCssAndJs();
+    document.body.style.fontSize = "14px";
+    let currentJsonStr
+    try {
+      let currentJson = jsonlint.parse(content)
+      currentJsonStr = JSON.stringify(currentJson)
+      result = new JSONFormat(content, 4).toString()
+    } catch (e) {
+      result = '<span style="color: #f1592a; font-weight: bold;">' + e + '</span>'
+      currentJsonStr = result
+    }
+    document.body.innerHTML = result;
+
+    addJsonOperation();
+  } else {
+  }
 })
+
+const insertCssAndJs = function () {
+  let style = document.createElement('link');
+  style.rel = 'stylesheet';
+  style.href = '//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
+  document.getElementsByTagName('head')[0].appendChild(style);
+
+  let script = document.createElement('script');
+  script.src = 'https://static.dei2.com/tools/js/jquery.json.js'
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+const addJsonOperation = function () {
+  let html = '<div style="position: fixed; right: 15px; top: 15px; min-width: 100px; height: 36px; background-color: red; z-index: 9999;">' +
+    '' +
+    '</div>'
+  document.body.innerHTML += html
+}
+
+const expandAll = function (currentJsonStr) {
+  currentJsonStr.trim() !== '' && (new JSONFormat(currentJsonStr, 4).showAll())
+}
+
+const compressAll = function (currentJsonStr) {
+  currentJsonStr.trim() !== '' && (new JSONFormat(currentJsonStr, 4).hideAll())
+}
