d0f282fe53461fa258fb367e79bf776fb5115607
liangshan <loveliangshan@126.com>
2018-07-18 19:44:03 +0800

    no message

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.querySelector('#test').onclick = function () {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     // chrome.tabs.executeScript(tabs[0].id, {code: 'document.body.style.backgroundColor="red";'});
-    chrome.tabs.executeScript(tabs[0].id, {file: 'content-script.js'});
+    chrome.tabs.executeScript(tabs[0].id, {file: 'js/popup_background.js'});
   });
 }
